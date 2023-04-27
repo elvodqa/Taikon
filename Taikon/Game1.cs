e7862a9a44f8a4ebad80e0a6b4a22ef9d0f4b910
elvodqa <emircengiz21@gmail.com>
2023-04-27 19:16:00 +0300

    CTRL+O Settings

--- Taikon/Game1.cs
+++ Taikon/Game1.cs
@@ -29,15 +29,6 @@ public class Game1 : Game
 
     private SongPlayer _songPlayer;
 
-    private string _mapSong;
-    private string _mapArtist;
-    private string _mapCreator;
-    private string _mapAudioFile;
-    private List<string> _mapObjects;
-    private string _mapFile = "./Songs/TestSong1/map1.tai";
-    private int _currentMapBpm;
-    private double _currentMapOffset;
-    private double _currentMapLength;
     private double _currentMapTime = 0;
     private bool _isPlaying = true;
 
@@ -65,7 +56,7 @@ public class Game1 : Game
 
     private GameState _gameState = GameState.MainMenu;
     private SpriteFontBase font18;
-    
+
     private Texture2D _donSplashTexture;
     private float _dontRotation = 0;
 
@@ -73,7 +64,14 @@ public class Game1 : Game
     private Map SelectedMap;
     private SubMap SelectedSubMap;
 
-    private bool _focusedSettings = false;
+    private bool _settingsAnimStart = false;
+    private bool _settingsCloseAnimStart = false;
+    private bool _settingsAnimDone = false;
+    private bool _settingsFocused = false;
+    private float _settingsAnimCurrent = 0;
+    private float _settingsAnimFinal = 300;
+    private Texture2D _dummyTexture;
+
     private bool _isVolumeChancing = false;
 
     public Game1()
@@ -86,27 +84,26 @@ public class Game1 : Game
         IsMouseVisible = true;
         Window.AllowUserResizing = true;
         Window.ClientSizeChanged += OnResize;
-        
     }
 
     protected override void Initialize()
     {
         _songPlayer = new();
-        
+
         foreach (var folder in Directory.GetDirectories("./Songs"))
         {
             Maps.Add(Map.ReadMaps(folder));
         }
-        
+
         var random = new Random();
         int index = random.Next(Maps.Count);
         SelectedMap = Maps[index];
         Console.WriteLine(SelectedMap.SubMaps.Count);
         int subIndex = random.Next(SelectedMap.SubMaps.Count);
         SelectedSubMap = SelectedMap.SubMaps[subIndex];
-        
+
         _songPlayer.Load($"{SelectedMap.FolderName}/{SelectedSubMap.AudioFile}");
-        _songPlayer.SetVolume(0.8f);
+        _songPlayer.SetVolume(0.1f);
         _songPlayer.Play();
         base.Initialize();
     }
@@ -117,6 +114,9 @@ public class Game1 : Game
         _fontSystem = new();
         _fontSystem.AddFont(File.ReadAllBytes("./Fonts/arial.ttf"));
         font18 = _fontSystem.GetFont(18);
+        
+        _dummyTexture = new Texture2D(GraphicsDevice, 1, 1);
+        _dummyTexture.SetData(new Color[] { Color.White });
 
         _mainMenuSettingsTexture = Content.Load<Texture2D>("UI/SETTINGS");
         _mainMenuEditorTexture = Content.Load<Texture2D>("UI/EDITOR");
@@ -136,115 +136,176 @@ public class Game1 : Game
 
         switch (_gameState)
         {
-        case GameState.MainMenu:
-        {
-            _dontRotation += 0.05f * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
-            // Play
-            if (Input.IsMouseHoveringOver(new Rectangle(10, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 - _mainMenuEditorTexture.Height - 10 - _mainMenuSettingsTexture.Height - 80, _mainMenuPlayTexture.Width, _mainMenuPlayTexture.Height), this))
+            case GameState.MainMenu:
             {
-                _onHoverMovePlay = 30;
-                if (!_isHoveringPlay)
+                _dontRotation += 0.05f * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+                // Play
+                if (Input.IsMouseHoveringOver(
+                        new Rectangle(10,
+                            Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 -
+                            _mainMenuEditorTexture.Height - 10 - _mainMenuSettingsTexture.Height - 80,
+                            _mainMenuPlayTexture.Width, _mainMenuPlayTexture.Height), this))
                 {
-                    _buttonHoverSound.Play();
-                    _isHoveringPlay = true;
-                }
-                if (Input.IsLeftMousePressed(true))
-                {
-                    _buttonClickSound.Play();
-                }
-            }
-            // Editor
-            else if (Input.IsMouseHoveringOver(new Rectangle(10, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 - _mainMenuEditorTexture.Height - 80, _mainMenuEditorTexture.Width, _mainMenuEditorTexture.Height), this))
-            {
-                _onHoverMoveEditor = 30;
-                if (!_isHoveringPlay)
-                {
-                    _buttonHoverSound.Play();
-                    _isHoveringPlay = true;
-                }
-                if (Input.IsLeftMousePressed(true))
-                {
-                    _buttonClickSound.Play();
-                }
-            }
-            // Settings
-            else if (Input.IsMouseHoveringOver(new Rectangle(10, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 80, _mainMenuSettingsTexture.Width, _mainMenuSettingsTexture.Height), this))
-            {
-                _onHoverMoveSettings = 30;
-                if (!_isHoveringPlay)
-                {
-                    _buttonHoverSound.Play();
-                    _isHoveringPlay = true;
-                }
-                if (Input.IsLeftMousePressed(true))
-                {
-                    _buttonClickSound.Play();
-                }
-            }
-            else
-            {
-                _onHoverMovePlay = 0;
-                _onHoverMoveEditor = 0;
-                _onHoverMoveSettings = 0;
-                _isHoveringPlay = false;
-            }
+                    _onHoverMovePlay = 30;
+                    if (!_isHoveringPlay)
+                    {
+                        _buttonHoverSound.Play();
+                        _isHoveringPlay = true;
+                    }
 
-            if (Input.IsKeyPressed(Keys.LeftControl, false))
-            {
-                if (Input.IsScrolled(Orientation.Down))
-                {
-                    _isVolumeChancing = true;
-                    if (_songPlayer.GetVolume() - 0.05f < 0)
+                    if (Input.IsLeftMousePressed(true))
                     {
-                        _songPlayer.SetVolume(0);
+                        _buttonClickSound.Play();
                     }
-                    else
-                    {
-                        _songPlayer.SetVolume(_songPlayer.GetVolume()-0.05f);
-                    }
-                    
                 }
-                else if (Input.IsScrolled(Orientation.Up))
+                // Editor
+                else if (Input.IsMouseHoveringOver(
+                             new Rectangle(10,
+                                 Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 -
+                                 _mainMenuEditorTexture.Height - 80, _mainMenuEditorTexture.Width,
+                                 _mainMenuEditorTexture.Height), this))
                 {
-                    _isVolumeChancing = true;
-                    if (_songPlayer.GetVolume() + 0.05f > 1)
+                    _onHoverMoveEditor = 30;
+                    if (!_isHoveringPlay)
                     {
-                        _songPlayer.SetVolume(1);
+                        _buttonHoverSound.Play();
+                        _isHoveringPlay = true;
                     }
-                    else
+
+                    if (Input.IsLeftMousePressed(true))
                     {
-                        _songPlayer.SetVolume(_songPlayer.GetVolume()+0.05f);
+                        _buttonClickSound.Play();
                     }
-                    
+                }
+                // Settings
+                else if (Input.IsMouseHoveringOver(
+                             new Rectangle(10, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 80,
+                                 _mainMenuSettingsTexture.Width, _mainMenuSettingsTexture.Height), this))
+                {
+                    _onHoverMoveSettings = 30;
+                    if (!_isHoveringPlay)
+                    {
+                        _buttonHoverSound.Play();
+                        _isHoveringPlay = true;
+                    }
+
+                    if (Input.IsLeftMousePressed(true))
+                    {
+                        _buttonClickSound.Play();
+                    }
                 }
                 else
                 {
-                    _isVolumeChancing = false;
+                    _onHoverMovePlay = 0;
+                    _onHoverMoveEditor = 0;
+                    _onHoverMoveSettings = 0;
+                    _isHoveringPlay = false;
+                }
+
+                if (Input.IsKeyPressed(Keys.LeftControl, false))
+                {
+                    if (Input.IsScrolled(Orientation.Down))
+                    {
+                        _isVolumeChancing = true;
+                        if (_songPlayer.GetVolume() - 0.05f < 0)
+                        {
+                            _songPlayer.SetVolume(0);
+                        }
+                        else
+                        {
+                            _songPlayer.SetVolume(_songPlayer.GetVolume() - 0.05f);
+                        }
+                    }
+                    else if (Input.IsScrolled(Orientation.Up))
+                    {
+                        _isVolumeChancing = true;
+                        if (_songPlayer.GetVolume() + 0.05f > 1)
+                        {
+                            _songPlayer.SetVolume(1);
+                        }
+                        else
+                        {
+                            _songPlayer.SetVolume(_songPlayer.GetVolume() + 0.05f);
+                        }
+                    }
+                    else
+                    {
+                        _isVolumeChancing = false;
+                    }
                 }
             }
-            
-        }
-            break;
-        case GameState.SongSelect:
-        {
-            
-        }
-            break;
-        case GameState.GamePlay:
-        {
-            if (_isPlaying)
+                break;
+            case GameState.SongSelect:
             {
-                _currentMapTime += gameTime.ElapsedGameTime.TotalMilliseconds;
             }
-        }
-            break;
-        case GameState.Editor:
-        {
-            
-        }
-            break;
+                break;
+            case GameState.GamePlay:
+            {
+                if (_isPlaying)
+                {
+                    _currentMapTime += gameTime.ElapsedGameTime.TotalMilliseconds;
+                }
+            }
+                break;
+            case GameState.Editor:
+            {
+            }
+                break;
         }
 
+        // Settings
+        if (Input.IsKeyPressed(Keys.LeftControl, false))
+        {
+            if (Input.IsKeyPressed(Keys.O, true))
+            {
+                if (!_settingsFocused)
+                {
+                    _settingsFocused = true;
+                    _settingsAnimStart = true;
+                }
+
+                if (_settingsFocused)
+                {
+                    _settingsCloseAnimStart = true;
+                }
+            }
+        }
+
+        if (_settingsAnimStart)
+        {
+            if (!_settingsAnimDone)
+            {
+                if (_settingsAnimCurrent != _settingsAnimFinal)
+                {
+                    _settingsAnimCurrent += 50;
+                }
+
+                if (_settingsAnimCurrent >= _settingsAnimFinal)
+                {
+                    _settingsAnimCurrent = _settingsAnimFinal;
+                    _settingsAnimDone = true;
+                    _settingsAnimStart = false;
+                }
+            }
+        }
+        /*
+        if (_settingsCloseAnimStart)
+        {
+            if (!_settingsAnimDone)
+            {
+                if (_settingsAnimCurrent != 0)
+                {
+                    _settingsAnimCurrent -= 50;
+                }
+
+                if (_settingsAnimCurrent < 0)
+                {
+                    _settingsAnimCurrent = 0;
+                    _settingsAnimDone = true;
+                    _settingsAnimStart = false;
+                }
+            }
+        } */
 
         Input.FixScrollLater();
         base.Update(gameTime);
@@ -253,45 +314,52 @@ public class Game1 : Game
     protected override void Draw(GameTime gameTime)
     {
         GraphicsDevice.Clear(Color.Black);
-        
+
         switch (_gameState)
         {
             case GameState.MainMenu:
             {
                 _spriteBatch.Begin();
                 // Draw menu buttons
-                _spriteBatch.Draw(_mainMenuSettingsTexture, new Vector2(10+_onHoverMoveSettings, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 80), Color.White);
-                _spriteBatch.Draw(_mainMenuEditorTexture, new Vector2(10+_onHoverMoveEditor, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 - _mainMenuEditorTexture.Height -80), Color.White);
-                _spriteBatch.Draw(_mainMenuPlayTexture, new Vector2(10+_onHoverMovePlay, Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 - _mainMenuEditorTexture.Height - 10 - _mainMenuSettingsTexture.Height - 80), Color.White);
-                
+                _spriteBatch.Draw(_mainMenuSettingsTexture,
+                    new Vector2(10 + _onHoverMoveSettings,
+                        Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 80), Color.White);
+                _spriteBatch.Draw(_mainMenuEditorTexture,
+                    new Vector2(10 + _onHoverMoveEditor,
+                        Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 -
+                        _mainMenuEditorTexture.Height - 80), Color.White);
+                _spriteBatch.Draw(_mainMenuPlayTexture,
+                    new Vector2(10 + _onHoverMovePlay,
+                        Window.ClientBounds.Height - 10 - _mainMenuPlayTexture.Height - 10 -
+                        _mainMenuEditorTexture.Height - 10 - _mainMenuSettingsTexture.Height - 80), Color.White);
+
                 // Don splash rotating at the middle
-                _spriteBatch.Draw(_donSplashTexture, new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2), null, Color.White, MathHelper.ToRadians(_dontRotation), new Vector2(_donSplashTexture.Width / 2, _donSplashTexture.Height / 2), 0.5f, SpriteEffects.None, 0f);
+                _spriteBatch.Draw(_donSplashTexture,
+                    new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2), null, Color.White,
+                    MathHelper.ToRadians(_dontRotation),
+                    new Vector2(_donSplashTexture.Width / 2, _donSplashTexture.Height / 2), 0.5f, SpriteEffects.None,
+                    0f);
 
                 if (_isVolumeChancing)
                 {
                     var vol = _songPlayer.GetVolume() * 100;
-                    _spriteBatch.DrawString(font18, $"Vol:{vol}", new(0, Window.ClientBounds.Height-20), Color.White);
+                    _spriteBatch.DrawString(font18, $"Vol:{vol}", new(0, Window.ClientBounds.Height - 20), Color.White);
                 }
-                
+
                 _spriteBatch.End();
             }
                 break;
             case GameState.SongSelect:
             {
-            
             }
                 break;
             case GameState.GamePlay:
             {
                 _spriteBatch.Begin();
-               
-                _spriteBatch.DrawString(font18, "Song: " + _mapSong, new Vector2(10, 10), Color.White);
-                _spriteBatch.DrawString(font18, "Artist: " + _mapArtist, new Vector2(10, 30), Color.White);
-                _spriteBatch.DrawString(font18, "Creator: " + _mapCreator, new Vector2(10, 50), Color.White);
-                _spriteBatch.DrawString(font18, "BPM: " + _currentMapBpm, new Vector2(10, 70), Color.White);
-                _spriteBatch.DrawString(font18, "Audio: " + _mapAudioFile, new Vector2(10, 90), Color.White);
+
                 _spriteBatch.DrawString(font18, "MapTime: " + _currentMapTime, new Vector2(10, 110), Color.White);
-                _spriteBatch.DrawString(font18, "SongTime: " + _songPlayer.GetPosition(), new Vector2(10, 130), Color.White);
+                _spriteBatch.DrawString(font18, "SongTime: " + _songPlayer.GetPosition(), new Vector2(10, 130),
+                    Color.White);
 
                 _spriteBatch.Draw(
                     _hitAreaCirleTexture,
@@ -305,11 +373,20 @@ public class Game1 : Game
                 break;
             case GameState.Editor:
             {
-            
             }
                 break;
         }
         
+        // Settings
+        if (_settingsFocused)
+        {
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_dummyTexture, 
+                new Rectangle((int)(-300+_settingsAnimCurrent), 0, 300, Window.ClientBounds.Height),
+                new Color(97, 42, 96, 200)
+                );
+            _spriteBatch.End();
+        }
 
         base.Draw(gameTime);
     }
@@ -321,6 +398,7 @@ public class Game1 : Game
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.ApplyChanges();
         }
+
         if (Window.ClientBounds.Height < 600)
         {
             _graphics.PreferredBackBufferHeight = 600;
